b80bc7e395f859ef392e8e0d933f13b46df7ea8f
Cristhian <trichains@hotmail.com>
2023-12-06 03:47:38 -0400

    att: fade-in-out

--- pages/index.js
+++ pages/index.js
@@ -19,18 +19,36 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-          <h1 className="h1">
+          <motion.h1
+            variants={fadeIn('down', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1">
             Transformando Ideias <br /> em{' '}
             <span className="text-accent">Realidade Digital</span>
-          </h1>
+          </motion.h1>
           {/* subtitle */}
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+          <motion.p
+            variants={fadeIn('down', 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
             addjasaskdasdjasdas
-          </p>
+          </motion.p>
           {/* btn */}
           <div className="flex justify-center xl:hidden relative">
             <ProjectsBtn />
           </div>
+          <motion.div
+            variants={fadeIn('down', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex">
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
       {/* image */}
