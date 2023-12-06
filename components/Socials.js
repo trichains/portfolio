d62d5a144b54d3e195cfbf03f0255cd485c78ba2
Cristhian <trichains@hotmail.com>
2023-12-06 11:20:16 -0400

    add: header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,31 @@
+// links
+import Link from 'next/link';
+
+// icons
+import {
+  RiLinkedinLine,
+  RiGithubLine,
+  RiFacebookLine,
+  RiInstagramLine
+} from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
