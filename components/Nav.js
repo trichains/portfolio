115d0b49c176cec6106cfc5474615b785259799f
Cristhian <trichains@hotmail.com>
2023-12-06 01:52:44 -0400

    nav finalizado

--- components/Nav.js
+++ components/Nav.js
@@ -42,9 +42,22 @@ const Nav = () => {
         {navData.map((link, index) => {
           return (
             <Link
-              className={`${link.path === pathname && 'text-accent'}`}
+              className={`${
+                link.path === pathname && 'text-amber-500'
+              } relative flex items-center group hover:text-amber-500 transition-all duration-300`}
               href={link.path}
               key={index}>
+              {/* tooltip */}
+              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex">
+                <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
+                  <div className="text-[12px] leading-none font-semibold capitalize">
+                    {link.name}
+                  </div>
+                  {/* triangle */}
+                  <div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2"></div>
+                </div>
+              </div>
+              {/* icon */}
               <div>{link.icon}</div>
             </Link>
           );
--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -4,7 +4,7 @@ const TopLeftImg = () => {
   return (
     <div
       className="absolute top-0 left-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px]
-      opacity-50">
+      opacity-40">
       <Image
         src="/top-left-img.png"
         alt="top-left-image"
