3800338d41583a67048c7d5785c2155d295a6a8b
Cristhian <trichains@hotmail.com>
2023-12-06 23:44:00 -0400

    att: link das redes

--- components/Socials.js
+++ components/Socials.js
@@ -10,20 +10,31 @@ import {
 } from 'react-icons/ri';
 
 const Socials = () => {
+  const ExternalLink = ({ href, children }) => {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-accent transition-all duration-300">
+        {children}
+      </a>
+    );
+  };
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <ExternalLink href={'https://www.linkedin.com/in/trichains/'}>
         <RiLinkedinLine />
-      </Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      </ExternalLink>
+      <ExternalLink href={'https://github.com/trichains'}>
         <RiGithubLine />
-      </Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      </ExternalLink>
+      <ExternalLink href={'https://www.facebook.com/trichains'}>
         <RiFacebookLine />
-      </Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      </ExternalLink>
+      <ExternalLink href={'https://www.instagram.com/trichains'}>
         <RiInstagramLine />
-      </Link>
+      </ExternalLink>
     </div>
   );
 };
