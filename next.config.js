80b31b690768d833b90a4fcf7d4a67c7c8d0f673
Cristhian <trichains@hotmail.com>
2023-12-06 22:14:17 -0400

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: isProd ? '/portfolio-trichains' : '',
-  assetPrefix: isProd ? '/portfolio-trichains/' : ''
-};
+}
+
+module.exports = nextConfig
